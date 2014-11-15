17015b6d385753fe573284dc35097ab73c000ad0
abeyuya <yuya.abe.0525@gmail.com>
2014-11-15 18:03:25 +0900

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,69 @@
+#
+# Cookbook Name:: middleware
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+########
+# apache
+########
+package "httpd" do
+  action :install
+end
+
+template "set apache conf includer" do
+  path "/etc/httpd/conf.d/virtualhost.conf"
+  source "virtualhost.conf"
+  # owner "apache"
+  # group "apache"
+  mode 0600
+end
+
+execute "mkdir /etc/httpd/conf.d/sites" do
+  command "mkdir -p /etc/httpd/conf.d/sites"
+  action :run
+end
+
+template "set wordpress apache conf" do
+  path "/etc/httpd/conf.d/sites/#{node["wordpress"]["project_name"]}.conf"
+  source "project_name.conf.erb"
+  # owner "apache"
+  # group "apache"
+  mode 0600
+  variables({
+    :install_path => node["wordpress"]["install_path"],
+    :project_name => node["wordpress"]["project_name"],
+  })
+end
+
+service "httpd" do
+  action [ :enable, :start ]
+end
+
+
+########
+# mysql
+########
+%w{mysql mysql-server mysql-devel}.each do |pkg_name|
+  package pkg_name do
+    action :install
+  end
+end
+
+service "mysqld" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+
+########
+# php
+########
+%w{php php-devel php-mbstring php-mysql php-pdo php-xml php-gd}.each do |pkg_name|
+  package pkg_name do
+    action :install
+  end
+end
